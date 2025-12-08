508c865cfe37a83baa40fc93285c6a4f7523c08f
gadje <gadje@sct-24-62cd-1371.univ-rouen.fr>
2025-12-08 10:36:11 +0100

    Create IceCream App with swift

---
+++ IceCream/IceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamApp.swift
+//  IceCream
+//
+//  Created by gadje on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
